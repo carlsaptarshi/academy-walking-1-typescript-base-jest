c6e6c39e39fd60da1268f25f0c179ba80572ae23
Carl Saptarshi <carl.saptarshi@cazoo.co.uk>
2021-04-30 15:16:20 +0100

    mob next [ci-skip] [ci skip] [skip ci]

--- src/test/example.test.ts
+++ src/test/example.test.ts
@@ -6,3 +6,11 @@ describe('example test', () => {
         expect(example.add(1, 2)).toBe(3);
     })
 })
+
+describe('[fizzbuzz]', () => {
+    it('should 1 and return 1', () =>{
+        const example: Example = new Example();
+        const result = example.fizzbuzz(1);
+        expect(result).toBe("1");
+    })
+})
