d048bf38685203d1ea9a4bde4033ad310b0a6a49
Rob Baile <robert.baile@cazoo.co.uk>
2021-05-07 14:33:18 +0100

    mob next [ci-skip] [ci skip] [skip ci]

---
+++ src/test/string-calculator.test.ts
@@ -0,0 +1,6 @@
+describe('string calculator tests', () => {
+    it("should take in an empty string and return 0", () => {
+        let stringCalculator: StringCalculator  = new StringCalculator();
+        expect(stringCalculator.add("")).toBe(0);
+    })
+})
