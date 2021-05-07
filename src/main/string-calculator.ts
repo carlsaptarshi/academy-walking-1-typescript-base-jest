31fb3040fb514d3ffe76b968188b3fa147763ab6
Rob Baile <robert.baile@cazoo.co.uk>
2021-05-07 16:54:25 +0100

    mob next [ci-skip] [ci skip] [skip ci]

--- src/main/string-calculator.ts
+++ src/main/string-calculator.ts
@@ -1,10 +1,14 @@
 export class StringCalculator {
     add(inputString: string): number {
-        const delimiter = inputString.startsWith("//") ? inputString[2] : ",";
+        const delimiter = this.getDelimiter(inputString);
 
         return inputString
             .replace(/\n/g, delimiter)
             .split(delimiter)
             .reduce((currentTotal: number, inputValue: string) => currentTotal + Number(inputValue), 0);
+    }
+
+    private getDelimiter(inputString: string) {
+        return inputString.startsWith("//") ? inputString[2] : ",";
     }
 }
