396d99b3e120dde080f886e7dead52591c9862e0
Andrew Garner <andrew@andrewgarner.com>
2021-05-07 14:36:09 +0100

    String calculator takes an empty string returns 0

--- src/test/string-calculator.test.ts
+++ src/test/string-calculator.test.ts
@@ -1,3 +1,9 @@
+class StringCalculator {
+    add(s: string): number {
+        return 0;
+    }
+}
+
 describe('string calculator tests', () => {
     it("should take in an empty string and return 0", () => {
         let stringCalculator: StringCalculator  = new StringCalculator();
